17ad81cb1538cb37f56dfcb9a41326292cf33428
Anoop Sharma <anoopshar@cadomain.local>
2018-04-12 01:19:00 +0530

    Added Form for Messages

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-var {generateMessage} = require('./utils/message.js');
+var {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '..', 'public');
 console.log(publicPath);
